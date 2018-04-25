dec137e3d571f06cd292c226a024dd26408c6262
Keita Suzuki <ksuzuki@cyber.t.u-tokyo.ac.jp>
2018-04-26 03:14:16 +0900

    DI 設定追加

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,27 +1,40 @@
 package codecheck;
 
 import org.apache.commons.lang3.math.NumberUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
 import codecheck.service.MainService;
 
 /**
  * メインクラス。
  */
+@Component
 public class App {
+	@Autowired
+	MainService mainService;
+	
 	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("META-INF/config.xml");
+		App p = context.getBean(App.class);
+		p.start(args);
+	}
+	
+	private void start(String[] args) {
 		if (args.length != 2 || !NumberUtils.isDigits(args[1])) {
 			System.out.println("error! invalid arguments.");
 			System.err.println("error! invalid arguments.");
-			return;
+			System.exit(1);
 		}
 		
 		String seed = args[0];
 		String n = args[1];
 		
-		MainService mainService = new MainService();
 		mainService.init(seed);
 		
 		System.out.println(mainService.calc(Integer.parseInt(n)));
-		return;
+		System.exit(0);
 	}
 }
